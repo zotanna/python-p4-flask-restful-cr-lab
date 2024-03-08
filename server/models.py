9909a1e297c1323726f388c65ef62a4bf7dc1630
denis <denniiskim@gmail.com>
2024-03-08 04:07:54 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
